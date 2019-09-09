5c5120293935a4db31fff491b40b349427e62f60
Alex Rocha <rocha8524@gmail.com>
2019-09-09 16:55:19 -0400

    Figuring out input in HTML Page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,40 +2,55 @@
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // Set Up Variables for game
-var computerLetter = letters[Math.floor(Math.random()*letters.length)];
-var userInput = null;
-var wins = 0;
-var losses = 0;
-var guesses = 10;
+var computerChoice; 
+var userWins = 0;
+var userLosses = 0;
 var chancesLeft = 10;
 var guessesMade = [];
 
+// Set up computer input
+var randomLetter = function() {
+computerChoice = letters[Math.floor(Math.random()*letters.length)];
+};
+
 // Set up user letter
 document.onekeyup = function(event) {
-    const newLocal = String.fromCharCode;
-    var userInput = newLocal(event.key);
-
-// Set up conditions for winning and losing
-    if (userInput == computerLetter) {
-        wins++;
-        Reset();
-    }  
-    
-    if (chancesLeft == 0) {
-        losses++;
-        Reset();
-    } 
-
-//Reset variables
-function Reset() {
-    chancesLeft = 10;
-    guessesMade = [];
-    computerLetter = letters[Math.floor(Math.random()*letters.length)];
+    var userInput = event.keyCode;
+    newFunction(userInput); 
 }
 
-// Link Javascript with HTML text.
-    document.getElementsById("wins").innerHTML = "Wins: ";
-    document.getElementsById("losses").innerHTML = "Losses: ";
-    document.getElementsById("chancesLeft").innerHTML = "Chances Left: ";
-    document.getElementsById("guessesMade").innerHTML = "Guesses Made: ";
+// Set up conditions for chances left.
+var guessesLeft = function() {
+    document.getElementById("chances-left").innerHTML = chancesLeft;
+    
+};
+
+// Set up text for guesses made.
+var madeChoice = function() {
+    document.getElementById("guesses-made").innerHTML = guessesMade;
+};
+
+// Set up conditions for winning
+    if (userInput == computerChoice) {
+        userWins++;
+        document.getElementsById("wins").innerHTML = userWins;
+        resetPsychic();
+    } 
+
+// Set up conditions for losing
+    else if (chancesLeft == 0) {
+        userLosses++;
+        document.getElementsById("losses").innerHTML = userLosses;
+        resetPsychic();
+    }; 
+
+// Reset game
+var resetPsychic = function() {
+    chancesLeft = 10;
+    guessesMade = [];
+    randomLetter();
+    };
+
+function newFunction(userInput) {
+    userInput.push;
 }
