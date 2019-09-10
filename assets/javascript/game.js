c9349e1ae7d3d07844e5cc41a1eb06341a698a46
Alex Rocha <rocha8524@gmail.com>
2019-09-10 19:05:04 -0400

    Fixing text in wins, losses and chances left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,58 @@
 // Letter choices
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Set Up Variables for game
-var computerChoice; 
-var userInput;
+var computerChoice;
 var userWins = 0;
 var userLosses = 0;
 var chancesLeft = 10;
+var guessesStart = 10;
 var guessesMade = [];
 
 // Set up computer input
-var randomLetter = function() {
-computerChoice = letters[Math.floor(Math.random()*letters.length)];
-};
-
-// Set up user letter
-var userInput = document.onekeyup = function() {
-    
-}
-
-// Set up conditions for chances left.
-var guessesLeft = function() {
-    document.getElementById("chances-left").innerHTML = chancesLeft;
-    
+var randomLetter = function () {
+    computerChoice = letters[Math.floor(Math.random() * (letters.length))];
 };
 
 // Set up text for guesses made.
-var madeChoice = function() {
-    document.getElementById("guesses-made").innerHTML = guessesMade;
+var madeChoices = function () {
+    document.getElementById("guesses-made").innerHTML = "Guesses Made: " + guessesMade;
 };
 
-// Set up conditions for winning
-    if (userInput == computerChoice) {
-        userWins++;
-        document.getElementById("wins").innerHTML = userWins;
-        resetPsychic();
-    } 
+// Set up conditions for chances left.
+var remainders = function () {
+    document.getElementById("chances-left").innerHTML = "Chances Left: " + chancesLeft;
+};
 
-// Set up conditions for losing
+// Set up user input
+document.onkeyup = function (event) {
+    var userChoice = event.key;
+    chancesLeft;
+    guessesMade.push(userChoice);
+    madeChoices();
+    guessesLeft();
+
+    // Set conditions for winning
+    if (userChoice == computerChoice) {
+        userWins++;
+        document.getElementById("wins").innerHTML = "Wins: " + userWins;
+        resetPsychicGame();
+    }
+
+    // Set conditions for losing
     else if (chancesLeft == 0) {
         userLosses++;
-        document.getElementById("losses").innerHTML = userLosses;
-        resetPsychic();
-    }; 
+        document.getElementById("losses").innerHTML = "Losses: " + userLosses;
+        resetPsychicGame();
+    }
 
-// Reset game
-var resetPsychic = function() {
-    chancesLeft = 10;
-    guessesMade = [];
-    randomLetter();
-    };
+    // Reset psychic game
+    var resetPsychicGame = function () {
+        guesses = [];
+        chancesLeft = 10;
+        newChoices();
+        madeChoices();
+        guessesLeft();
+    }
+
+};
